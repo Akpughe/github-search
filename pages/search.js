fe47bbb48f3d4e6a6e4e6a9bc8fd42b0706bb3b1
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-19 12:04:52 +0100

    search page (targeting values)

--- pages/search.js
+++ pages/search.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Searchbar from '../components/Searchbar';
 import Button from '../components/Button';
+import { useRouter } from 'next/dist/client/router';
 
 const search = () => {
+  const router = useRouter();
+  const [input, setInput] = useState('');
+
+  const onSearch = (e) => {
+    e.preventDefault();
+
+    const term = input;
+
+    if (!term) return;
+
+    router.push(`/result?term=${term}`);
+  };
   return (
     <>
+      <Head>
+        <title>Github Search</title>
+      </Head>
       <Navbar />
-      <div className="flex flex-col justify-center items-center h-screen">
-        <div>
-          <img src="/logo.svg" alt="" />
-        </div>
-        <div className="sm:w-2/5 w-full">
-          <Searchbar height="h-[60px]" />
-        </div>
-        <Link href="/result">
+      <form action="">
+        <div className="flex flex-col justify-center items-center h-screen">
           <div>
-            <Button text="Search Github" />
+            <img src="/logo.svg" alt="" />
           </div>
-        </Link>
-      </div>
+          <div className="sm:w-2/5 w-full">
+            <Searchbar
+              text={input}
+              change={(e) => setInput(e.target.value)}
+              height="h-[60px]"
+            />
+          </div>
+          <div>
+            <Button search={onSearch} text="Search Github" />
+          </div>
+        </div>
+      </form>
     </>
   );
 };
