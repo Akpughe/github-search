be276770ca0025bdf0567f53976bf5fe2a7328c6
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-19 12:06:14 +0100

    icon

---
+++ components/icons/ChevronRight.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ChevronRight = () => {
+  return (
+    <svg
+      width="8"
+      height="14"
+      viewBox="0 0 8 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      transform=" rotate(360)"
+    >
+      <path
+        d="M1 1L7 7L1 13"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ChevronRight;
