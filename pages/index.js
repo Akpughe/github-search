63bb089b08afc82fb1f38d68fa6e93f74deb5029
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-19 12:05:10 +0100

    login

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,13 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Button from '../components/Button';
+import { useRouter } from 'next/dist/client/router';
+import { useSession, signIn } from 'next-auth/client';
 
 export default function Home() {
+  const [session, loading] = useSession();
+  const router = useRouter();
+  
   return (
     <>
       <Head>
@@ -12,9 +17,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="flex flex-col items-center justify-center h-screen">
+      {!loading ? (
+        <div className="flex flex-col items-center justify-center h-screen">
+          {!session ? (
+            <div onClick={signIn}>
+              <Button text="Login to Github" />
+            </div>
+          ) : (
+            router.push('/search')
+          )}
+        </div>
+      ) : null}
+
+      {/* <div className="flex flex-col items-center justify-center h-screen">
         <Button text="Login to Github" />
-      </div>
+      </div> */}
+
+      {/* <LoginGithub
+        clientId="4f262cc9e20d3043da02"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        // redirectUri='/search'
+      /> */}
     </>
   );
 }
