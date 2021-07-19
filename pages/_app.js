cd802f8418fd37c6137a9f9b794bb25dbaf19023
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-19 12:31:25 +0100

    added components and progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,34 @@
 import '../styles/globals.css';
 import '../styles/tailwind.css';
+import { Provider as AuthProvider } from 'next-auth/client';
+import Head from 'next/head'
+import NProgress from 'nprogress';
+import Router from 'next/dist/client/router';
+
+const showProgressBar = (delay) => {
+  const timer = setTimeout(() => NProgress.start(), delay);
+  Router.events.on('routeChangeComplete', () => stopProgress(timer));
+  Router.events.on('routeChangeError', () => stopProgress(timer));
+};
+
+const stopProgress = (timer) => {
+  clearTimeout(timer);
+  NProgress.done();
+};
+
+Router.events.on('routeChangeStart', () => showProgressBar(300));
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider session={pageProps.session}>
+       <Head>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="/css/nprogress.css" />
+        <title>Github Search</title>
+      </Head>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
