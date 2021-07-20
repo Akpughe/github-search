9f6b87f2baa37f138ec07f461ef8d5acfc9cc6c0
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-21 00:01:02 +0100

    dropdown for logout

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,12 +2,19 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/dist/client/link';
 import { useRouter } from 'next/dist/client/router';
 import Searchbar from './Searchbar';
-import { useSession } from 'next-auth/client';
+import { useSession, getSession ,signOut } from 'next-auth/client';
 
 const Navbar = () => {
   const [session, loading] = useSession();
   const router = useRouter();
+  const [show, setShow] = useState(false);
   const [input, setInput] = useState('');
+
+  const handleShow = () => {
+    setShow(!show);
+  };
+
+
   const onSearch = (e) => {
     e.preventDefault();
 
@@ -19,30 +26,43 @@ const Navbar = () => {
   };
 
   return router.pathname === `/search` ? (
-    <nav className="flex justify-end  w-full bg-white h-20  ">
-      <div className="flex items-center px-16 cursor-pointer">
-        <div className=" h-9 w-9 rounded-full ">
-          <img src={session && session.user.image} alt="" />
+    <>
+      <nav className="flex justify-end  w-full bg-white h-20  ">
+        <div className="flex items-center px-16 cursor-pointer">
+          <div className=" h-9 w-9 rounded-full ">
+            {/* <img src={session && session.user.image} alt="" /> */}
+          </div>
+          <div className="mr-2 ml-2">
+            <span>{session && session.user.name}</span>
+          </div>
+          <div onClick={handleShow}>
+            <svg
+              width="12"
+              height="7"
+              viewBox="0 0 12 7"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.7156 0.300562C11.5278 0.11125 11.2722 0.00476399 11.0056 0.00476399C10.7389 0.00476399 10.4833 0.11125 10.2956 0.300562L6.00556 4.57056L1.70556 0.290563C1.31344 -0.0987982 0.679923 -0.09656 0.290562 0.295562C-0.0987984 0.687684 -0.0965597 1.3212 0.295562 1.71056L5.29556 6.71056C5.4859 6.89409 5.74119 6.99477 6.00556 6.99056C6.2679 6.98946 6.5193 6.88531 6.70556 6.70056L11.7056 1.70056C12.0895 1.31445 12.0939 0.692123 11.7156 0.300562Z"
+                fill="black"
+              />
+            </svg>
+          </div>
+          {show && (
+            <div className="bubble absolute top-[70px] right-[50px] w-48 h-12 text-center border py-2">
+              {session ? (
+                <span onClick={signOut} className="text-[#FF1733] ">
+                  Logout
+                </span>
+              ) : (
+                router.push('/')
+              )}
+            </div>
+          )}
         </div>
-        <div className="mr-2 ml-2">
-          <span>{session && session.user.name}</span>
-        </div>
-        <div>
-          <svg
-            width="12"
-            height="7"
-            viewBox="0 0 12 7"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M11.7156 0.300562C11.5278 0.11125 11.2722 0.00476399 11.0056 0.00476399C10.7389 0.00476399 10.4833 0.11125 10.2956 0.300562L6.00556 4.57056L1.70556 0.290563C1.31344 -0.0987982 0.679923 -0.09656 0.290562 0.295562C-0.0987984 0.687684 -0.0965597 1.3212 0.295562 1.71056L5.29556 6.71056C5.4859 6.89409 5.74119 6.99477 6.00556 6.99056C6.2679 6.98946 6.5193 6.88531 6.70556 6.70056L11.7056 1.70056C12.0895 1.31445 12.0939 0.692123 11.7156 0.300562Z"
-              fill="black"
-            />
-          </svg>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   ) : (
     <nav className="flex justify-between w-full bg-white h-20 shadow-lg  ">
       <Link href="/search">
@@ -63,10 +83,10 @@ const Navbar = () => {
 
       <div className="sm:flex hidden items-center px-16 cursor-pointer">
         <div className=" h-9 w-9 rounded-full ">
-          <img src={session && session.user.image} alt="" />
+          {/* <img src={session && session.user.image} alt="" /> */}
         </div>
         <div className="mr-2 ml-2">
-          <span>{session && session.user.name}</span>
+          {/* <span>{session && session.user.name}</span> */}
         </div>
         <div>
           <svg
@@ -88,3 +108,12 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      session: await getSession(context)
+    }
+  }
+}
