bc562170a744dfe50e15c7e7b0950a381f6cd4a5
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-21 00:09:57 +0100

    styling

--- components/Repos.js
+++ components/Repos.js
@@ -4,7 +4,7 @@ const Repos = ({ id, count, f_name, desc, star, lang }) => {
   return (
     <div>
       {/* <h1 className=" font-bold text-xl ">{count} repository results</h1> */}
-      <div className=" bg-white w-[680px] h-auto shadow-lg mt-7" key={id}>
+      <div className=" bg-white sm:w-[680px] w-auto h-auto shadow-lg mt-7" key={id}>
         <div className="p-5">
           <h1 className="font-bold text-base">{f_name}</h1>
           <p className=" font-normal text-sm text-[#91929E] ">{desc}</p>
