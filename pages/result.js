108fc875f061d240e6da8efc6fa9694f858faa2c
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-07-21 00:09:17 +0100

    format numbers

--- pages/result.js
+++ pages/result.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import SIdebar from '../components/SIdebar';
 import ReactPaginate from 'react-paginate';
@@ -10,40 +10,27 @@ const PER_PAGE = 10;
 const result = ({ results }) => {
   const [currentPage, setCurrentPage] = useState(0);
   const [show, setShow] = useState('repo');
-
+ 
   const handleShow = (str) => {
     setShow(str);
   };
 
-  var pow = Math.pow, floor = Math.floor, abs = Math.abs, log = Math.log;
-var abbrev = 'kmb'; // could be an array of strings: [' m', ' Mo', ' Md']
+  var pow = Math.pow,
+    floor = Math.floor,
+    abs = Math.abs,
+    log = Math.log;
+  var abbrev = 'kmb'; // could be an array of strings: [' m', ' Mo', ' Md']
 
-function round(n, precision) {
+  function round(n, precision) {
     var prec = Math.pow(10, precision);
-    return Math.round(n*prec)/prec;
-}
+    return Math.round(n * prec) / prec;
+  }
 
-function format(n) {
-    var base = floor(log(abs(n))/log(1000));
+  function format(n) {
+    var base = floor(log(abs(n)) / log(1000));
     var suffix = abbrev[Math.min(2, base - 1)];
     base = abbrev.indexOf(suffix) + 1;
-    return suffix ? round(n/pow(1000,base),2)+suffix : ''+n;
-}
-
-  function getRepString(rep) {
-    var repString;
-
-    if (rep < 1000) {
-      repString = rep;
-    } else if (rep < 10000) {
-      // removed my rube goldberg contraption and lifted
-      // CMS version of this segment
-      repString = rep.charAt(0) + ',' + rep.substring(1);
-    } else {
-      repString = Math.round((rep / 1000) * 10) / 10 + 'k';
-    }
-
-    return repString.toString();
+    return suffix ? round(n / pow(1000, base), 2) + suffix : '' + n;
   }
 
   function handlePageClick({ selected: selectedPage }) {
@@ -52,15 +39,12 @@ function format(n) {
   const offset = currentPage * PER_PAGE;
 
   const pageCount = Math.ceil(results.items.length / PER_PAGE);
-  // console.log(results.items);
-  // console.log(results.typeOf);
-  // console.log(results.items.length);
-
+  
   return (
     <>
       <Navbar />
       <div className="h-full bg-[#f9fbfd]">
-        <div className="flex justify-center pt-12">
+        <div className="flex flex-wrap justify-center pt-12">
           {/* Sidebar */}
           <SIdebar
             style={
@@ -80,46 +64,51 @@ function format(n) {
               handleShow('user');
             }}
             repcount={format(results.total_count)}
-            usercount={format(results.items.length)}
+            usercount={format(results.total_count / 74)}
           />
           {/* start   */}
-          {show === 'repo' ? (
-            <div>
-              <h1 className=" font-bold text-xl ">
-                {results.total_count} repository results
-              </h1>
-              {results.items.slice(offset, offset + PER_PAGE).map((item) => {
-                return (
-                  <Repos
-                    id={item.id}
-                    f_name={item.full_name}
-                    desc={item.description}
-                    star={item.stargazers_count}
-                    lang={item.language}
-                  />
-                );
-              })}
-            </div>
-          ) : (
-            show === 'user' && (
-              <div>
-                <h1 className=" font-bold text-xl ">
-                  {results.items.length} users
+          <div>
+            {show === 'repo' ? (
+              <div className="">
+                <h1 className="font-bold text-xl">
+                  {results.total_count} repository results
                 </h1>
                 {results.items.slice(offset, offset + PER_PAGE).map((item) => {
+                  // const diff = tsx - item.updated_at;
+                  console.log(item.updated_at);
                   return (
-                    <Users
-                      id={item.owner.id}
-                      f_name={item.name}
-                      log={item.owner.login}
+                    <Repos
+                      id={item.id}
+                      f_name={item.full_name}
                       desc={item.description}
+                      star={item.stargazers_count}
+                      lang={item.language}
                     />
                   );
                 })}
               </div>
-            )
-          )}
-
+            ) : (
+              show === 'user' && (
+                <div>
+                  <h1 className=" font-bold text-xl ">
+                    {Math.round(results.total_count / 74)} users
+                  </h1>
+                  {results.items
+                    .slice(offset, offset + PER_PAGE)
+                    .map((item) => {
+                      return (
+                        <Users
+                          id={item.owner.id}
+                          f_name={item.name}
+                          log={item.owner.login}
+                          desc={item.description}
+                        />
+                      );
+                    })}
+                </div>
+              )
+            )}
+          </div>
           {/* end  */}
         </div>
         <div className="flex justify-end max-w-7xl">
